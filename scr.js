37b4d8b4c6125b09e5b80f113d2d6c22b007d212
Karthick V <108870718+karthicK007V@users.noreply.github.com>
2022-08-20 13:48:28 +0530

    Add files via upload

--- scr.js
+++ scr.js
@@ -1,6 +1,6 @@
 async function har(){
     try{
-    let thi=await fetch("http://hp-api.herokuapp.com/api/characters");
+    let thi=await fetch("https://hp-api.herokuapp.com/api/characters");
     let res=await thi.json();
     console.log(fi(res));
     
